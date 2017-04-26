7e442eee6f3b975e661948b485463b800153e7fd
Kyle Pfromer <kpfromer2@gmail.com>
2017-04-26 15:08:42 -0600

    Create TokenDao Test

--- src/main/java/com/smarttodo/dao/TokenDao.java
+++ src/main/java/com/smarttodo/dao/TokenDao.java
@@ -9,10 +9,7 @@ import org.springframework.stereotype.Repository;
  * Created by kpfromer on 4/25/17.
  */
 
-//todo: create test
 @Repository
 public interface TokenDao extends CrudRepository<VerificationToken, Long> {
     VerificationToken findByToken(String token);
-
-    VerificationToken findByUser(User user);
 }
