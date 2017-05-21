42ecfff950fe382da432e574a7e7122f622ea7af
Kyle Pfromer <kpfromer2@gmail.com>
2017-05-21 10:08:50 -0400

    Removed clutter

--- src/main/java/com/smarttodo/web/controller/ErrorController.java
+++ src/main/java/com/smarttodo/web/controller/ErrorController.java
@@ -25,34 +25,6 @@ public class ErrorController extends AbstractErrorController {
         super(errorAttributes);
     }
 
-
-    /**
-     * Just catching the {@linkplain ResourceNotFoundException} exceptions and render
-     * the 404.jsp error page.
-     */
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public String notFound() {
-        return "notFound";
-    }
-
-    /**
-     * Responsible for handling all errors and throw especial exceptions
-     * for some HTTP status codes. Otherwise, it will return a map that
-     * ultimately will be converted to a json error.
-     */
-    //todo: add test
-//    @RequestMapping(ERROR_PATH)
-//    public ResponseEntity<?> handleErrors(HttpServletRequest request) {
-//        HttpStatus status = getStatus(request);
-//
-//        if (status.equals(HttpStatus.NOT_FOUND))
-//            throw new ResourceNotFoundException();
-//
-//        return ResponseEntity.status(status).body(getErrorAttributes(request, false));
-//    }
-
-    //todo: create test
     @RequestMapping(ERROR_PATH)
     public String handleErrors(HttpServletRequest request, Model model) {
         HttpStatus status = getStatus(request);
